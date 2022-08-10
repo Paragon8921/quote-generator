2895592cf5cdf2911a3c67862e8ecfb64c3a514c
Clark Siller <clark@odesso.com>
2022-08-09 17:10:18 -0700

    Finished Quote Project

--- script.js
+++ script.js
@@ -1,24 +1,91 @@
+///////////////////////////////////
+// Fetch API to retrieve quotes  //
+///////////////////////////////////
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+// Show loading
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function hideLoadingSpinner() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 
 // Show New Quote
 function newQuote() {
+  showLoadingSpinner();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  //Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  hideLoadingSpinner();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+  showLoadingSpinner();
   const apiURL = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiURL);
 
     apiQuotes = await response.json();
+
     newQuote();
   } catch (error) {
     // Catch Error Here
   }
 }
 
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
 getQuotes();
+
+///////////////////////////////////////////////////////////
+// Use local quotes "quotes.js" to generate quotes       //
+///////////////////////////////////////////////////////////
+
+// // Show new quote
+// function newQuote() {
+//   // Pick a random quote from "quotes.js"
+//   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//   console.log(quote);
+// }
+
+// newQuote();
